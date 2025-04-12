563cb73d380b8b289d7948c329044560d548a222
= <=>
2025-04-12 17:33:14 -0300

    Modified code for GameJamExample scene

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,61 @@
-import { Color4, Vector3 } from '@dcl/sdk/math'
-import { EasingFunction, engine, Entity, Material, MeshCollider, MeshRenderer, Transform, TransformType, Tween } from '@dcl/sdk/ecs'
+import { Color4, Quaternion, Vector3 } from '@dcl/sdk/math'
+import { EasingFunction, engine, Material, MeshCollider, MeshRenderer, Transform, Tween } from '@dcl/sdk/ecs'
+import { setupUi } from './ui'
 
-let playerPos: Vector3
-let box: Entity
+let wonLevel: boolean = false
 
-function getEntityPosition(entity: Entity) {
-    const playerPosition = Transform.get(entity).position
+export function getPlayerPosition() {
+    const player = engine.PlayerEntity
+    const playerPosition = Transform.get(player).position
+
+    console.log('Player position:', playerPosition.x, playerPosition.y, playerPosition.z)
     return playerPosition
-} 
+}
 
-function getDistancebetweenVectors(a: Vector3, b: Vector3) {
-    return Math.sqrt(
-        Math.pow(a.x - b.x, 2) +
-        Math.pow(a.y - b.y, 2) +
-        Math.pow(a.z - b.z, 2)
-    )
+export function isLevelWon() {
+    const playerY = getPlayerPosition().y
+    if (playerY > 6)  {
+        wonLevel = true
+    } else {
+        wonLevel = false
+    }
+    return wonLevel
 }
 
 export function main() {
-
-    box = engine.addEntity()
+    const box = engine.addEntity()
 
     Transform.create(box, {
-        position: Vector3.create(7.5, 0.5, 7.5),
-        // rotation: Vector3.create(0, 0, 0),
-        // scale: Vector3.create(1, 1, 1),
+        position: Vector3.create(5, 0.25, 5), 
+        rotation:   Quaternion.create(0, 0, 0),
+        scale: Vector3.create(2, 0.5, 2), // change to shape of the box
     })
-    
+
     MeshRenderer.setBox(box)
 
     Material.setPbrMaterial(box, {
-        albedoColor: Color4.Red(),
+        albedoColor: Color4.Blue(),
         metallic: 1,
-        roughness: 0.5,
+        roughness: 0.1,
     })
 
     MeshCollider.setBox(box)
 
-    const boxTransform = Transform.getMutable(box)
-
-    boxTransform.scale.x = 3
-    boxTransform.scale.z = 3
+    // Special type of system : Tween, its easir to define, 
+    //it. only modified transform components.
 
     Tween.create(box, {
-        duration: 10000,
+        duration: 15000,
         easingFunction: EasingFunction.EF_LINEAR,
-        currentTime: 0,
-        playing: true,
         mode: Tween.Mode.Move({
-            start: Vector3.create(15, 0.5, 7.15),
-            end: Vector3.create(1, 0.5, 1),
-        }),
+            start: Vector3.create(5, 0.5, 5),
+            end: Vector3.create(5, 6, 5),
+        })
     })
+    engine.addSystem(getPlayerPosition)
+    engine.addSystem(isLevelWon)
+    setupUi()
 
-    const playerPos = getEntityPosition(engine.PlayerEntity)
-
-    console.log("Player position: ", playerPos.x, playerPos.y, playerPos.z)
-
-    console.log("Distance to box : ", getDistancebetweenVectors(playerPos, boxTransform.position))
-
-    //////// Box grabbed from the creator hub /////////\
-
-    const box2 = engine.getEntityOrNullByName("box")
-
-    if (box2) {
-        console.log("Box entity found!")
-    } 
 }
 
-export function getDistancewithBox() {
-    const playerPos = getEntityPosition(engine.PlayerEntity)
-    const boxTransform = Transform.getMutable(box)
-
-    const distance = getDistancebetweenVectors(playerPos, boxTransform.position)
-    
-    if (distance < 4) {
-
-        Material.setPbrMaterial(box, {
-            albedoColor: Color4.White()
-        })
-        MeshCollider.setBox(box)
-    
-        console.log("Player is close to the box!")
-    } else {
-        MeshCollider.setBox(box)
-        Material.setPbrMaterial(box, {
-            albedoColor: Color4.Red()
-        })
-    }
-}
-
-engine.addSystem(getDistancewithBox)
+export var uiLevelWon: boolean = wonLevel
