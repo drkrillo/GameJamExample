82d437a4dec5276eb1bafdfd42c736dcd021dd9a
= <=>
2025-04-13 15:15:08 -0300

    Simplified code by leaving only one system

--- src/index.ts
+++ src/index.ts
@@ -2,60 +2,46 @@ import { Color4, Quaternion, Vector3 } from '@dcl/sdk/math'
 import { EasingFunction, engine, Material, MeshCollider, MeshRenderer, Transform, Tween } from '@dcl/sdk/ecs'
 import { setupUi } from './ui'
 
-let wonLevel: boolean = false
+let levelWon: boolean = false
 
-export function getPlayerPosition() {
-    const player = engine.PlayerEntity
-    const playerPosition = Transform.get(player).position
+export function isGameWon() {
+    const playerY = Transform.get(engine.PlayerEntity).position.y
 
-    console.log('Player position:', playerPosition.x, playerPosition.y, playerPosition.z)
-    return playerPosition
-}
-
-export function isLevelWon() {
-    const playerY = getPlayerPosition().y
-    if (playerY > 6)  {
-        wonLevel = true
-    } else {
-        wonLevel = false
+    if (playerY > 5) {
+        levelWon = true
+        console.log(String(levelWon))
     }
-    return wonLevel
+    return levelWon
 }
 
 export function main() {
     const box = engine.addEntity()
 
     Transform.create(box, {
-        position: Vector3.create(5, 0.25, 5), 
-        rotation:   Quaternion.create(0, 0, 0),
-        scale: Vector3.create(2, 0.5, 2), // change to shape of the box
+        position: Vector3.create(5, 0.25, 5),
+        rotation: Quaternion.create(0, 0, 0),
+        scale: Vector3.create(2, 0.5, 2)
     })
 
     MeshRenderer.setBox(box)
 
+    MeshCollider.setBox(box)
+
     Material.setPbrMaterial(box, {
-        albedoColor: Color4.Blue(),
-        metallic: 1,
+        albedoColor: Color4.Yellow(),
+        metallic: 0.8, 
         roughness: 0.1,
     })
 
-    MeshCollider.setBox(box)
-
-    // Special type of system : Tween, its easir to define, 
-    //it. only modified transform components.
-
     Tween.create(box, {
-        duration: 15000,
+        duration: 10000,
         easingFunction: EasingFunction.EF_LINEAR,
         mode: Tween.Mode.Move({
-            start: Vector3.create(5, 0.5, 5),
-            end: Vector3.create(5, 6, 5),
+            start: Vector3.create(5, 0.25, 5),
+            end: Vector3.create(7, 5, 7)
         })
     })
-    engine.addSystem(getPlayerPosition)
-    engine.addSystem(isLevelWon)
+
+    engine.addSystem(isGameWon)
     setupUi()
-
 }
-
-export var uiLevelWon: boolean = wonLevel
