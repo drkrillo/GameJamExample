6abe2069c9b8f63937446564f4aa13f4d18948e6
= <=>
2025-04-13 21:25:11 -0300

    Add PointerEventSystem

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
 import { Color4, Quaternion, Vector3 } from '@dcl/sdk/math'
-import { EasingFunction, engine, Material, MeshCollider, MeshRenderer, Transform, Tween } from '@dcl/sdk/ecs'
-import { setupUi } from './ui'
+import { EasingFunction, engine, Material, MeshCollider, MeshRenderer, pointerEventsSystem, Transform, Tween, tweenSystem } from '@dcl/sdk/ecs'
 
-let levelWon: boolean = false
+let gameFinished: boolean = false
 
-export function isGameWon() {
-    const playerY = Transform.get(engine.PlayerEntity).position.y
 
-    if (playerY > 5) {
-        levelWon = true
-        console.log(String(levelWon))
+function getPlayerY() {
+    const player = engine.PlayerEntity
+    const playerTransform = Transform.get(player)
+    const playerY = playerTransform.position.y
+
+    console.log(String(playerY))
+    return playerY
+}
+
+function updateGameStatus() {
+    const playerY = getPlayerY()
+
+    if (playerY > 14) {
+        gameFinished = true
     }
-    return levelWon
 }
 
 export function main() {
+
     const box = engine.addEntity()
 
     Transform.create(box, {
-        position: Vector3.create(5, 0.25, 5),
+        position: Vector3.create(1, 0.5, 1), 
         rotation: Quaternion.create(0, 0, 0),
-        scale: Vector3.create(2, 0.5, 2)
+        scale: Vector3.create(2, 0.25, 2),
     })
 
     MeshRenderer.setBox(box)
@@ -29,19 +37,34 @@ export function main() {
 
     Material.setPbrMaterial(box, {
         albedoColor: Color4.Yellow(),
-        metallic: 0.8, 
+        metallic: 0.7, 
         roughness: 0.1,
     })
 
-    Tween.create(box, {
-        duration: 10000,
-        easingFunction: EasingFunction.EF_LINEAR,
-        mode: Tween.Mode.Move({
-            start: Vector3.create(5, 0.25, 5),
-            end: Vector3.create(7, 5, 7)
-        })
-    })
+    const button = engine.getEntityOrNullByName('button')
 
-    engine.addSystem(isGameWon)
-    setupUi()
+    if (button) {
+        pointerEventsSystem.onPointerDown(
+            {
+                entity: button,
+                opts: {
+                    hoverText: "click!"
+                }
+            },
+            function () {
+    
+                Tween.create(box, {
+                    duration: 30000,
+                    easingFunction: EasingFunction.EF_LINEAR,
+                    mode: Tween.Mode.Move({
+                        start: Vector3.create(1, 0.5, 1),
+                        end: Vector3.create(15, 15, 15),
+                    })
+                })
+    
+            }
+        )
+    }
+
+    engine.addSystem(updateGameStatus)
 }
